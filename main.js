fe702423d388d1426a9fb3d8a5dd16ebdc6f9a6d
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-09-16 10:45:58 +0300

    Task 1 completed

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+// ********************Task 1
+class Book {
+  title = "Java Script and jQuery";
+  author = "John Duckett";
+  pages = 640;
+  displayInfo() {
+    console.log(
+      `Название книги ${this.title}, автор ${this.author}, количество страниц ${this.pages}.`
+    );
+  }
+}
 
+const book = new Book();
+book.displayInfo();
+// *******************Task 2
