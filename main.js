990713995493246a72d85ea1a509427b647fcd5f
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-09-16 10:53:54 +0300

    Task 2 completed

--- main.js
+++ main.js
@@ -13,3 +13,19 @@ class Book {
 const book = new Book();
 book.displayInfo();
 // *******************Task 2
+class Student {
+  constructor(name, age, grade) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+  displayInfo() {
+    console.log(`Name:${this.name}\nAge:${this.age}\nGrade:${this.grade}`);
+  }
+}
+
+const student1 = new Student("John Smith", 16, "10th grade");
+student1.displayInfo();
+
+const student2 = new Student("Jane Doe", 17, "11th grade");
+student2.displayInfo();
